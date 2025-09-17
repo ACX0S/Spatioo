125248f111e2f0f724a3c28f84195751b16e7f43
ACX0S <leoparpis@gmail.com>
2025-09-17 19:06:39 -0300

    Alteração no placeholder do Telefone página Cria conta

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -291,6 +291,7 @@ const Login = () => {
                   className="mr-2 mt-1"
                   onChange={(e) => setAgreeTerms(e.target.checked)}
                 />
+                {/* Termos de uso e Política de Privacidade da Spatioo */}
                 <label htmlFor="terms" className="text-xs text-muted-foreground">
                   Concordo com os <a href="#" className="text-spatioo-green">Termos de Uso</a> e <a href="#" className="text-spatioo-green">Política de Privacidade</a> da Spatioo.
                 </label>
